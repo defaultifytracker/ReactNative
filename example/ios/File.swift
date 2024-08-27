8c4c7524aa0862cb12a1c72bd062cc25a7ae73da
User <user@admins-Laptop.local>
2024-08-27 14:03:41 +0530

    chore: initial commit

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  DefaultifyExample
-//
-
-import Foundation
--- ios/Defaultify.swift
+++
@@ -1,8 +0,0 @@
-@objc(Defaultify)
-class Defaultify: NSObject {
-
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
-}
